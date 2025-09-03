b4720f6e562fd03929cf3d423765d7cfb74b4c36
Huzaifa Karim <karimhuzaifa590@gmail.com>
2025-09-03 13:18:47 +0500

    Solved build issue

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
+  // SPA mode is better for Puter-based applications
+  // as all APIs are client-side (auth, storage, AI)
   ssr: false,
 } satisfies Config;
